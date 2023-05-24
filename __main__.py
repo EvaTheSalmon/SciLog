a691d1dbafc4db23d7df9273ea9373d366d53709
Oleg <18466855+EvaTheSalmon@users.noreply.github.com>
2023-05-25 01:01:40 +0300

    Dinamic form name update in progress

--- __main__.py
+++ __main__.py
@@ -1,4 +1,4 @@
-import os
+import os, logging
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 from dotenv import load_dotenv
@@ -26,7 +26,9 @@ def add():
 
 @app.route('/save-form-data', methods=['POST'])
 def save_form_data():
-    result = jsonify(request.form)
+    # result = jsonify(request.form.to_dict())
+    result = jsonify(request.form.to_dict())
+    logging.debug(result)
     return result
 
 if __name__ == '__main__':
